3350322d2b09d5d99c6569a4958ae8d6d404543e
Tim Helmke <timhelmke@aol.com>
2021-11-10 10:02:02 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <PasswordGenerator msg="Welcome to my PasswordGenerator" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PasswordGenerator from "./components/PasswordGenerator.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    PasswordGenerator,
   },
 };
 </script>
